f715a970a1f084f0c0e3bd2fb0e66409e0d780fc
Stefan Blandin <stefanblandin@yahoo.com>
2021-07-12 11:49:12 -0700

    Fixing eslint errors

--- src/UI/app.js
+++ src/UI/app.js
@@ -1,66 +1,21 @@
-mpos = {x:0,y:0}
-var ctx = {};
-var clicked_lm = 0;
+mpos = {x: 0, y: 0};
+const ctx = {};
 
-// var canvas = document.getElementById('canvas');
-// if (canvas.getContext) 
-// {
-// 	ctx = canvas.getContext('2d');
-// }
-
-
-function main(){
-	// document.body.addEventListener('mousedown', function(){
-	// 	if (mpos.x<canvas.width && mpos.y<canvas.height && mpos.x>0 && mpos.y>0)
-	// 	clicked_lm=1;
-	// }, true); 
-
-	// document.body.addEventListener('mouseup', function(){
-	// 	clicked_lm=3;
-	// }, true); 
-
-	// $("canvas").mousemove(function(e) {
-	// 	mpos.x = e.pageX - $('canvas').offset().left;
-	// 	mpos.y = e.pageY - $('canvas').offset().top;
-	// })
-
-	// draw();
+/** main function, executes click event bindings and so forth. */
+function main() {
 }
 
-// function draw(){
-// 	ctx.clearRect(0, 0, canvas.width, canvas.height)
-// 	ctx.fillStyle="white"
-// 	ctx.fillRect(0,0,canvas.width,canvas.height)
-
-// 	requestAnimationFrame(draw);
-// }
-
-function detectmob() { 
- if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
-    return true;
-  }
- else {
-    return false;
-  }
-}
 window.onresize = function(event) {
-resizeDiv();
-}
+  resizeDiv();
+};
 
-function resizeDiv() {
+/** executes when the window is resized. use for responsiveness */
+const resizeDiv = function() {
+  vpw = $(window).width();
+  vph = $(window).height();
 
-	vpw = $(window).width();
-	vph = $(window).height();
+  const m=detectmob();
+};
 
-	var m=detectmob()
-}
-
-$(document).ready(resizeDiv)
-$(document).ready(main)
+$(document).ready(resizeDiv);
+$(document).ready(main);
--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
 // this is the code that "Runs" the extension.
 // This is seperate from the code that runs the UI inside the extension.
 
-var deliberatelybroke = true
+const deliberatelybroke = true;
