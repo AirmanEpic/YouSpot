a0e2bf937e07604c8c5acb50f8ef652a33a76100
indila5 <87392308+indila5@users.noreply.github.com>
2021-07-13 12:56:19 -0700

    updated app.js
    Deliberately breaking code standards

--- src/UI/app.js
+++ src/UI/app.js
@@ -1,20 +1,19 @@
-mpos = {x: 0, y: 0};
+mpos = { x: 0, y: 0 };
 const ctx = {};
 
-/** main function, executes click event bindings and so forth. */
 function main() {
 }
 
-window.onresize = function(event) {
+window.onresize = function (event) {
   resizeDiv();
 };
 
 /** executes when the window is resized. use for responsiveness */
-const resizeDiv = function() {
+const resizeDiv = function () {
   vpw = $(window).width();
   vph = $(window).height();
 
-  const m=detectmob();
+  const m = detectmob();
 };
 
 $(document).ready(resizeDiv);
