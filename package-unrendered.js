addfa238b4d7e76d29cbbd0f391d3e6075000ada
jordan <jbax1899@yahoo.com>
2021-07-15 17:16:01 -0400

    Update package-unrendered.js

--- package-unrendered.js
+++ package-unrendered.js
@@ -3,4 +3,4 @@ Uses Browserify to bundle the package.
 Note - do not use 'var' here as it will incorrectly scope the variable.
 For Jbax - node C:\Users\jbax1\AppData\Roaming\npm\node_modules\browserify\bin\cmd.js package-unrendered.js -o package.js
 */
-spotifyapi = require("spotify-web-api-js");
+spotifyapi = require('spotify-web-api-js');
