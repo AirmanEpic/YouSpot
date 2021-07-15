2d0360687146a52bd541df2d991ca72a4386ad7d
jordan <jbax1899@yahoo.com>
2021-07-15 17:07:43 -0400

    Browserify + Lambda server test POST call
    New node module spotify-web-api-js.
    New node module browserify, used to package the Spotify api for browser.
    Test ajax POST call to the lambda server.

--- src/UI/app.js
+++ src/UI/app.js
@@ -3,7 +3,30 @@ const ctx = {};
 
 /** main function, executes click event bindings and so forth. */
 function main() {
-  $('.content').text('Hey JBax, change this to search for Rick Astley');
+  //This POST is run on execution - It records the response given by the server, if any.
+  $.ajax({
+    url: 'https://ndwr1ks2f9.execute-api.us-east-2.amazonaws.com/default/YouSpot/',
+    type: 'post',
+    dataType: 'html',
+    data: {
+       //body stuff here, not needed
+    },
+    headers: {
+        'x-api-key': 'GgmDMVSvcm5XpGCbHsZHhXwzQT6DYyS5S9SXrw39', 
+    },
+    success: function (data) {
+      console.log('POST success!');
+      console.log(data);
+    },
+    error: function (data) {
+      console.log('POST error!');
+      console.log(data);
+    },
+    fail: function (data) {
+      console.log('POST fail!');
+      console.log(data);
+    }
+  });
 }
 
 /** executes the resizediv function when the window is resized */
