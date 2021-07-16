532ce1765f56a54e443fd9277ef0d73b7b5f6574
Stefan Blandin <stefanblandin@yahoo.com>
2021-07-15 18:19:39 -0700

    fixing an issue with the default send commands

--- backend/index.js
+++ backend/index.js
@@ -75,7 +75,7 @@ exports.handler = async (event, context) => {
     //   default:
     //     throw new Error(`Unsupported method "${event.httpMethod}"`);
     // }
-    const recData = event.body;
+    const recData = JSON.parse(event.body);
     if (recData) {
       [body, statusCode] = router(recData);
     }
