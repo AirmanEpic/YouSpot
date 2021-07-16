b678a452719d6878694e264bd25814a5d64262ad
Scrippe <shawnbasnett@gmail.com>
2021-07-16 17:59:19 -0500

    Created Music Detection
    Detects music by checking the meta elements for the word "music " in video tags, then the title, then the description. If it does not find the word in one of the elements, it checks the next.

--- src/content.js
+++ src/content.js
@@ -1,4 +1,34 @@
 // this is the code that "Runs" the extension.
 // This is seperate from the code that runs the UI inside the extension.
-
 const deliberatelybroke = true;
+
+var videoTags = $("meta[name='keywords']").attr('content');
+var videoTitle = $("meta[name='title']").attr('content').toLowerCase();
+var videoDesc = $("meta[name='description']").attr('content');
+var isMusic;
+
+for (let i = 0; i < 3; i++)
+{
+    switch (i) {
+        case 0:
+            if (videoTags.split("music").length!=1) 
+            { 
+                isMusic = true; i = 3;
+            }
+            break;
+        case 1:
+            if (videoTitle.split("music").length!=1) 
+            {
+                isMusic = true; i = 3;
+            } 
+            break;
+        case 2:
+            if (videoDesc.split("music").length!=1) 
+            {
+                isMusic = true;
+            } 
+            else {isMusic = false};
+            break;
+    }
+}
+console.log("is music = " + isMusic);
