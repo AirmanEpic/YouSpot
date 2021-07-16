215f5c65c94213536552a38caf9cfe5fbfbfdddf
Scrippe <shawnbasnett@gmail.com>
2021-07-16 18:14:08 -0500

    More lint fixes
    Lint fixing using powershell

--- src/content.js
+++ src/content.js
@@ -2,33 +2,30 @@
 // This is seperate from the code that runs the UI inside the extension.
 const deliberatelybroke = true;
 
-let videoTags = $("meta[name='keywords']").attr('content').toLowerCase();
-let videoTitle = $("meta[name='title']").attr('content').toLowerCase();
-let videoDesc = $("meta[name='description']").attr('content').toLowerCase();
+const videoTags = $('meta[name=\'keywords\']').attr('content').toLowerCase();
+const videoTitle = $('meta[name=\'title\']').attr('content').toLowerCase();
+const videoDesc = $('meta[name=\'description\']').attr('content').toLowerCase();
 let isMusic;
 
 for (let i = 0; i < 3; i++) {
-    switch (i) 
-    {
-        case 0:
-            if (videoTags.split("music").length!=1) 
-            { 
-                isMusic = true; i = 3;
-            }
-            break;
-        case 1:
-            if (videoTitle.split("music").length!=1) 
-            {
-                isMusic = true; i = 3;
-            } 
-            break;
-        case 2:
-            if (videoDesc.split("music").length!=1) 
-            {
-                isMusic = true;
-            } 
-            else {isMusic = false};
-            break;
-    }
+  switch (i) {
+    case 0:
+      if (videoTags.split('music').length!=1) {
+        isMusic = true; i = 3;
+      }
+      break;
+    case 1:
+      if (videoTitle.split('music').length!=1) {
+        isMusic = true; i = 3;
+      }
+      break;
+    case 2:
+      if (videoDesc.split('music').length!=1) {
+        isMusic = true;
+      } else {
+        isMusic = false;
+      };
+      break;
+  }
 }
-console.log("is music = " + isMusic);
+console.log('is music = ' + isMusic);
