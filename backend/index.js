16387bb683e763b9ee97ac1a0b036d84783e65ae
Stefan Blandin <stefanblandin@yahoo.com>
2021-07-15 17:54:35 -0700

    Error fixing try 2

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ exports.handler = async (event, context) => {
 
   let body = 'pepperoni pizza\n';
   let statusCode = '200';
-  let headers = {
+  const headers = {
     'Content-Type': 'text/plain',
   };
 
@@ -81,7 +81,7 @@ exports.handler = async (event, context) => {
     }
   } catch (err) {
     statusCode = '400';
-    body = JSON.stringify(err)
+    body = err;
   } finally {
     try {
       body = JSON.stringify(body);
